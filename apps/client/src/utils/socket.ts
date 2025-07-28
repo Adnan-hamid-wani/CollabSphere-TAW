b7742778585740724259f444a24f4b9426fa1dd3
Adnan-hamid-wani <Adnanaduuu@gmail.com>
2025-07-28 23:44:31 +0530

    Update socket.ts

--- apps/client/src/utils/socket.ts
+++ apps/client/src/utils/socket.ts
@@ -1,16 +1,20 @@
 import { io } from "socket.io-client";
 import { useNotificationStore } from "../store/notificationStore";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 const auth = JSON.parse(localStorage.getItem("auth") || "{}");
 
-const socket = io("http://localhost:4000", {
+const socket = io(API_URL, {
   autoConnect: true,
   withCredentials: true,
+  transports: ["websocket"],
   query: {
     userId: auth?.user?.id,
     role: auth?.user?.role,
   },
 });
+
 socket.on("notification", (data) => {
   const message = typeof data === "string" ? data : data?.message;
   if (message) {
